e6aa1fc794db7736a7761e78093ff4ad156c886c
Guigozones <guilhermeoliveira7962@gmail.com>
2026-01-12 15:36:24 -0300

    add: contador de números e funcionalidade ao botão new game

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 let computerNumber
 let userNumbers = []
+let attempts = 0
+let maxAttempts = 10
 
 function init() {
     computerNumber = Math.floor(Math.random() * 100) + 1
@@ -8,16 +10,36 @@ function init() {
 
 }
 
+document.getElementById('newGameButton').addEventListener('click', function() {
+  location.reload();
+});
+
 function compareNumbers() {
     const userNumber = Number(document.getElementById('inputBox').value)
     userNumbers.push(' ' + userNumber)
     document.getElementById('guesses').innerHTML = userNumbers
+
+    if(attempts >= maxAttempts) {
+        document.getElementById('textOutput').innerHTML = 'You have reached the maximum number of attempts. The number was ' + computerNumber
+        document.getElementById('inputBox').setAttribute('disabled', 'disabled')
+        return
+    }
     if (userNumber > computerNumber) {
         document.getElementById('textOutput').innerHTML = 'Your guess ' + userNumber + ' is too high'
+        document.getElementById('inputBox').value = ''
+        attempts++
+        document.getElementById('attempts').innerHTML = attempts
 
     }else if (userNumber < computerNumber) {
         document.getElementById('textOutput').innerHTML = 'Your guess ' + userNumber + ' is too low'
+        document.getElementById('inputBox').value = ''
+        attempts++
+        document.getElementById('attempts').innerHTML = attempts
+
     }else {
         document.getElementById('textOutput').innerHTML = 'Congratulations! You guessed the number ' + computerNumber
+        attempts++
+        document.getElementById('attempts').innerHTML = attempts
+        document.getElementById('inputBox').setAttribute('disabled', 'disabled')
     }
 }
